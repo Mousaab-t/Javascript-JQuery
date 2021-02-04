789555018d6fba48135e097bcd5049eda6773413
Mousaab-t <62936827+Mousaab-t@users.noreply.github.com>
2021-02-04 16:52:20 +0100

    Delete source.js

--- Calcul imc/source.js
+++
@@ -1,9 +0,0 @@
-// Afficher l'IMC
-function Traitement() {
-    var poids = document.getElementById("txtPoids").value
-    var taille = document.getElementById("txtTaille").value
-}
-
-this.CalculeIMC = function () {
-    return this.Poids / (this.Taille ** 2)
-}
